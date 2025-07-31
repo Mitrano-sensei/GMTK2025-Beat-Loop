f87d13d012bfec73ca163271a55646744b6060c6
Elyas Tigre <56512461+Mitrano-sensei@users.noreply.github.com>
2025-07-31 14:21:15 +0200

    Movements + Tileset (#1)
    * Removing .idea
    
    * Idle Animation
    
    * Movement Animation
    
    * Sprites foule
    
    ---------
    
    Co-authored-by: Skywiig <mattlorival1@gmail.com>

---
+++ Assets/_Project/Scripts/Player/PlayerAnimationController.cs
@@ -0,0 +1,93 @@
+using System.Threading.Tasks;
+using PrimeTween;
+using UnityEngine;
+
+public class PlayerAnimationController : MonoBehaviour
+{
+    [SerializeField] private Transform playerBodyTransform;
+    [SerializeField] private Transform modelTransform;
+
+    [Header("Idle")]
+    [SerializeField] private TweenSettings<Vector3> idleJiggleTweenSettings;
+    [SerializeField] private TweenSettings<Vector3> idleBreatheTweenSettings;
+    
+    [Header("Movement")]
+    [SerializeField] private TweenSettings movementTweenSettings;
+    
+    private Sequence _idleTween;
+    public bool IsMoving { get; private set; }
+
+    private void Start()
+    {
+        SetupTweens();
+    }
+
+    private void OnValidate()
+    {
+        // Kill the sequences
+        _idleTween.Stop();
+        SetupTweens();
+    }
+
+
+    private void SetupTweens()
+    {
+        SetupIdleTween();
+    }
+    
+    #region Idle
+    
+    private void SetupIdleTween()
+    {
+        if (_idleTween.isAlive)
+            return;
+        
+        // If we are in the editor, we don't want to play the idle animation
+        if (!Application.isPlaying)
+            return;
+        
+        _idleTween = Sequence.Create(cycles: -1, CycleMode.Yoyo).Group(Tween.LocalRotation(modelTransform, idleJiggleTweenSettings)).Group(Tween.Scale(modelTransform, idleBreatheTweenSettings));
+    }
+
+    public void StartIdleAnimation()
+    {
+        if (!_idleTween.isAlive)
+            SetupIdleTween();
+
+        if (!_idleTween.isPaused)
+            return;
+        
+        _idleTween.Complete();
+        _idleTween.isPaused = false;
+    }
+    
+    public void StopIdleAnimation()
+    {
+        if (!_idleTween.isAlive)
+            return;
+
+        if (_idleTween.isPaused)
+            return;
+        
+        _idleTween.Complete();  
+        _idleTween.Stop();
+        _idleTween.isPaused = true;
+    }
+    
+    #endregion
+    
+    #region Movement
+
+    public async Task MoveTo(Vector3 targetWorldPosition)
+    {
+        if (targetWorldPosition == playerBodyTransform.position)
+            return;
+        
+        IsMoving = true;
+        await Tween.Position(playerBodyTransform, targetWorldPosition, settings: movementTweenSettings);
+        modelTransform.position = targetWorldPosition;
+        IsMoving = false;
+    }
+    
+    #endregion
+}
